af6f1be712c77ae2e33a81cdeb8dae4f25d7cb00
PAGIDALA <ShivaShankar.PAGIDALA@geodis.com>
2020-07-24 16:23:36 -0500

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library.Models;
+
+namespace Library.Controllers
+{
+    public class BookController : Controller
+    {
+        BookModel bookDB = new BookModel();
+        public ActionResult Index()
+        {
+            return View(bookDB.GetBooksList());
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Book bookObj)
+        {
+            int i = bookDB.SaveBook(bookObj);
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            Book bookObj = bookDB.GetBookById(id);
+            return View(bookObj);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Book bookObj)
+        {
+            int i = bookDB.EditBookById(bookObj);
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(bookObj);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            int i = bookDB.DeleteBookById(id);
+            return RedirectToAction("index");
+        }
+
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public MVCEntities db = new MVCEntities();
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Register register)
+        {
+            db.Registers.Add(register);
+            int i = db.SaveChanges();
+            if (i>0)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return View(register);
+            }
+            
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
